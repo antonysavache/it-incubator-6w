ad013276ea3c0dff9570bb152d0c1cab7cb2a62d
antonysavache <89783847+antonysavache@users.noreply.github.com>
2025-02-10 18:58:42 +0100

    add watcher

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import {app} from "./app";
+import {SETTINGS} from "./configs/settings";
+import {connectToDatabase} from "./shared/infrastructures/db/mongo-db";
+
+
+async function startApp() {
+    try {
+        await connectToDatabase();
+        console.log('Connected to MongoDB');
+
+
+        app.listen(SETTINGS.PORT, () => {
+            console.log(`Server started on port: ${SETTINGS.PORT}`);
+        });
+    } catch (e) {
+        console.log('Server error:', e);
+        process.exit(1);
+    }
+}
+
+startApp();
